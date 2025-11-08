d172714fb12be50737b467f66715f521e146fa55
DavidBonvin <ledave.musique@gmail.com>
2025-11-08 17:31:37 +0100

    Updates

--- assets/md.transition-legacy-1iJz78sb.js
+++ assets/md.transition-legacy-1iJz78sb.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-70Xo_DlG.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-fN1O9ZRq.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
